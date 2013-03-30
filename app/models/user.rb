aea84e7697649a33151eb84ae9b1fec1a6e4f901
Doni Nikolovski <kfar11@yahoo.com>
2013-03-30 17:04:08 +0000

    Comlete chapter 7 exercises

--- app/models/user.rb
+++ app/models/user.rb
@@ -26,6 +26,7 @@ class User < ActiveRecord::Base
     VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
     validates :email, presence: true, format:       { with: VALID_EMAIL_REGEX },
                                       uniqueness:   { case_sensitive: false }
-    validates :password, presence: true, length: { minimum: 6 }
+    # We don't check for presence, because that's done for password_digest above
+    validates :password, length: { minimum: 6 }
     validates :password_confirmation, presence: true
 end
--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -40,6 +40,31 @@ describe "UserPages" do
             it "should create a user" do
                 expect { click_button submit }.to change(User, :count).by(1)
             end
+
+            describe "after saving the user" do
+                before { click_button submit }
+                let(:user) { User.find_by_email('user@example.com') }
+
+                it { should have_selector('title', text: user.name) }
+                it { should have_selector('div.alert.alert-success', text: 'Welcome') }
+            end
+        end
+
+        describe "after submission" do
+            before { click_button submit }
+
+            it { should have_selector('title', text: 'Sign up') }
+            it { should have_content('error') }
+        end
+
+        describe "with missing passwords" do
+            before do
+                fill_in "Name",     with: "Example User"
+                fill_in "Email",    with: "user@example.com"
+            end
+
+            it { should have_selector('title', text: 'Sign up') }
+            it { should have_content('Password') }
         end
     end
 end
