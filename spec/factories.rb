00b37abb199e7590f420f412f4e823b582e7fa92
Doni Nikolovski <kfar11@yahoo.com>
2013-03-30 11:57:41 +0000

    Commit orphaned files

---
+++ spec/factories.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+    factory :user do
+        name        "Winston Churchill"
+        email       "wchurchill@gov.co.uk"
+        password    "beaches"
+        password_confirmation "beaches"
+    end
+end
