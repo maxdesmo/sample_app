661646518efbff94f5a7a0a34854296f72878839
Doni Nikolovski <kfar11@yahoo.com>
2013-03-31 00:22:36 +0000

    Finish sign in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,13 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  # Helpers are only included in views by default, so we need to
+  # explicitly include it here if we want to use some of its functions
+  # in out controllers
+  include SessionsHelper
+
+  # Force signout to prevent CSRF attacks
+  def handle_unverified_request
+    sign_out
+    super
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -8,25 +8,33 @@
 #  created_at      :datetime         not null
 #  updated_at      :datetime         not null
 #  password_digest :string(255)
+#  remember_token  :string(255)
 #
 
 class User < ActiveRecord::Base
-    attr_accessible :name, :email, :password, :password_confirmation
-    # Adds the password and password_confirmation attrs, requires the
-    # presence of the password, requires that they match, and adds an
-    # authenticate method to compare an encrypted password to the
-    # password_digest to authenticate users. All that for free with
-    # 1 line of code!
-    has_secure_password
+  attr_accessible :name, :email, :password, :password_confirmation
+  # Adds the password and password_confirmation attrs, requires the
+  # presence of the password, requires that they match, and adds an
+  # authenticate method to compare an encrypted password to the
+  # password_digest to authenticate users. All that for free with
+  # 1 line of code!
+  has_secure_password
 
-    # The ! applies the change to itself
-    before_save { email.downcase! }
+  # The ! applies the change to itself
+  before_save { email.downcase! }
+  before_save :create_remember_token
 
-    validates :name, presence: true, length: { maximum: 50 }
-    VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-    validates :email, presence: true, format:       { with: VALID_EMAIL_REGEX },
-                                      uniqueness:   { case_sensitive: false }
-    # We don't check for presence, because that's done for password_digest above
-    validates :password, length: { minimum: 6 }
-    validates :password_confirmation, presence: true
+  validates :name, presence: true, length: { maximum: 50 }
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, format: { with: VALID_EMAIL_REGEX },
+                    uniqueness: { case_sensitive: false }
+  # We don't check for presence, because that's done for password_digest above
+  validates :password, length: { minimum: 6 }
+  validates :password_confirmation, presence: true
+
+  private
+
+    def create_remember_token
+      self.remember_token = SecureRandom.urlsafe_base64
+    end
 end
