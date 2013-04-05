d57e98e603dabafb52195233da35875ca3fcc0fc
Doni Nikolovski <kfar11@yahoo.com>
2013-04-05 20:13:54 +0100

    Add user following

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
-  before_filter :signed_in_user,  only: [:index, :edit, :update, :destroy]
+  before_filter :signed_in_user,  
+                only: [:index, :edit, :update, :destroy, :following, :followers]
   before_filter :correct_user,    only: [:edit, :update]
   before_filter :admin_user,      only: :destroy
 
@@ -58,6 +59,22 @@ class UsersController < ApplicationController
       flash[:notify] = "Admin can't delete own account!"
       redirect_to(root_path)
     end
+  end
+
+  # The following and followers pages are nearly identical, so 
+  # we render the same page with different dynamic elements
+  def following
+    @title = "Following"
+    @user = User.find(params[:id])
+    @users = @user.followed_users.paginate(page: params[:page])
+    render 'show_follow'
+  end
+
+  def followers
+    @title = "Followers"
+    @user = User.find(params[:id])
+    @users = @user.followers.paginate(page: params[:page])
+    render 'show_follow'
   end
 
   private
--- app/models/user.rb
+++ app/models/user.rb
@@ -22,6 +22,16 @@ class User < ActiveRecord::Base
   # microposts are dependent on their associated user, so they will
   # be destroyed when their associated user is destroyed
   has_many :microposts, dependent: :destroy
+  # We explicitely state the foreign key relationship because the id field
+  # cannot be inferred from the class/table name
+  has_many :relationships, foreign_key: "follower_id", dependent: :destroy
+  # Since the plural of followed is followeds, we change it to followed_users
+  # and make sure rails knows that by specifying source
+  has_many :followed_users, through: :relationships, source: :followed
+  has_many :reverse_relationships, foreign_key: "followed_id",
+                                   class_name:  "Relationship",
+                                   dependent:   :destroy
+  has_many :followers, through: :reverse_relationships, source: :follower
 
   # The ! applies the change to itself
   before_save { email.downcase! }
@@ -37,7 +47,21 @@ class User < ActiveRecord::Base
 
   def feed
     #This is prelimianry
-    Micropost.where("user_id = ?", id)
+    #Micropost.where("user_id = ?", id)
+    Micropost.from_users_followed_by(self)
+  end
+
+  def following?(other_user)
+    self.relationships.find_by_followed_id(other_user.id)
+  end
+
+  def follow!(other_user)
+    # self is implicit, so does not need to be specified
+    relationships.create!(followed_id: other_user.id)
+  end
+
+  def unfollow!(other_user)
+    relationships.find_by_followed_id(other_user.id).destroy
   end
 
   private
